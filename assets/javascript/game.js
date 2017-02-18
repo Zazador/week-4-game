54ff46d1d4bce1377ff116a0685c6af623c7a1e1
Zach Zador <zazador@gmail.com>
2017-02-17 22:32:40 -0600

    base game working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,20 +17,75 @@ $(document).ready(function(){
 	$("#blue").click(function() {
   		currentScore += blueValue;
   		$("#totalScore").text(currentScore);
-
+  		checkScore();
 	});
 
 	$("#purple").click(function() {
-  		alert(purpleValue);
+  		currentScore += purpleValue;
+  		$("#totalScore").text(currentScore);
+  		checkScore();
 	});
 
 	$("#green").click(function() {
-  		alert(greenValue);
+  		currentScore += greenValue;
+  		$("#totalScore").text(currentScore);
+  		checkScore();
 	});
 
 	$("#yellow").click(function() {
-  		alert(yellowValue);
+  		currentScore += yellowValue;
+  		$("#totalScore").text(currentScore);
+  		checkScore();
 	});
+
+
+	//Function to check if game is won
+	function checkScore() {
+		console.log("Current Score: " + currentScore);
+		if (currentScore === randomNum) {
+			gameWin();
+		} else if (currentScore < randomNum) {
+
+		} else {
+			gameLose();
+		}
+	}
+
+	//Function to handle a user winning the game
+	function gameWin() {
+		wins++;
+		$("#wins").text(wins);
+
+		blueValue = Math.floor(Math.random() * (12 - 1)) + 1;
+		purpleValue = Math.floor(Math.random() * (12 - 1)) + 1;
+		greenValue = Math.floor(Math.random() * (12 - 1)) + 1;
+		yellowValue = Math.floor(Math.random() * (12 - 1)) + 1;
+
+		randomNum = Math.floor(Math.random() * (120 - 19)) + 19;
+
+		currentScore = 0;
+
+		$("#randomNum").text(randomNum);
+		$("#totalScore").text(currentScore);
+	}
+
+	//Function to handle a user losing the game
+	function gameLose() {
+		losses++;
+		$("#losses").text(losses);
+
+		blueValue = Math.floor(Math.random() * (12 - 1)) + 1;
+		purpleValue = Math.floor(Math.random() * (12 - 1)) + 1;
+		greenValue = Math.floor(Math.random() * (12 - 1)) + 1;
+		yellowValue = Math.floor(Math.random() * (12 - 1)) + 1;
+
+		randomNum = Math.floor(Math.random() * (120 - 19)) + 19;
+
+		currentScore = 0;
+
+		$("#randomNum").text(randomNum);
+		$("#totalScore").text(currentScore);
+	}
 
 
 
